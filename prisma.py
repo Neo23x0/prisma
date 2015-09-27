89b380fc80bf79feb66e9eaa799be6fafeb42f6c
Neo23x0 <venom14@gmail.com>
2015-09-26 23:17:33 -0600

    Better regular expressions and colors

--- prisma.py
+++ prisma.py
@@ -33,17 +33,23 @@ class Prisma():
 
     # Predefined colors
     base_color = Fore.WHITE+Back.BLACK+Style.NORMAL
+
     cyan_highlight = Fore.CYAN+Back.BLACK+Style.BRIGHT
     green_highlight = Fore.GREEN+Back.BLACK+Style.BRIGHT
+    yellow_highlight = Fore.YELLOW+Back.BLACK+Style.BRIGHT
+    magenta_highlight = Fore.MAGENTA+Back.BLACK+Style.BRIGHT
+    blue_highlight = Fore.BLUE+Back.BLACK+Style.BRIGHT
 
     cyan_back = Fore.BLACK+Back.CYAN
     green_back = Fore.WHITE+Back.GREEN
     magenta_back = Fore.WHITE+Back.MAGENTA
+    blue_back = Fore.WHITE+Back.BLUE
 
     red = Fore.RED+Back.BLACK
     yellow = Fore.YELLOW+Back.BLACK
     green = Fore.GREEN+Back.BLACK
     magenta = Fore.MAGENTA+Back.BLACK
+    blue = Fore.BLUE+Back.BLACK
     grey = Fore.BLACK+Back.BLACK+Style.BRIGHT
 
     # Dynamic color allocation
@@ -61,50 +67,77 @@ class Prisma():
                 # General
                 {'name': 'uppercase_keywords', 'regex': r'\b([A-Z]{3,})\b', 'color': cyan_highlight},
                 {'name': 'keys_logline', 'regex': r'([A-Z_]{2,}:[\s\t])', 'color': cyan_highlight},
-                {'name': 'service', 'regex': r'(\s[A-Za-z]+\[[0-9]+\]:)', 'color': green_highlight},
+                {'name': 'service', 'regex': r'(\b[A-Za-z]+\[[0-9]+\]:)', 'color': blue_back},
                 {'name': 'string', 'regex': r'(\'[^\']+\')', 'color': yellow},
-                {'name': 'id', 'regex': r'(\[[0-9+]+\])', 'color': green},
+                {'name': 'id_num', 'regex': r'(\[[0-9]+\])', 'color': green},
+                {'name': 'id_alpha', 'regex': r'(\[[a-zA-Z\-]+\])', 'color': green_highlight},
                 {'name': 'tag', 'regex': r'(<[a-zA-Z_]+>)', 'color': green_back},
-                {'name': 'key', 'regex': r'\b([a-zA-Z]{2,}[a-zA-Z0-9_]+[=:])([\s\w\t])', 'color': green},
+                {'name': 'key', 'regex': r'\b(["]?[a-zA-Z]{2,}[a-zA-Z0-9_]+["]?[\s]?[=:])([\s\w\t])',
+                 'color': blue_highlight},
 
                 # Specific
                 {'name': 'exclamation', 'regex': r'((?![0-9]{2}m) [A-Za-z0-9\-\s\']+!)$', 'color': red},
-                {'name': 'date', 'regex': r'\b([A-Z][a-z][a-z][\s]?[\s][0-9]{1,2}[\s][0-9:]+)(\s[0-9]{4}|)\b', 'color': magenta_back, 'mode': 'mark_all'},
-                {'name': 'IP', 'regex': r'([1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9])', 'color': yellow },
+                {'name': 'date1', 'regex': r'\b([A-Z][a-z][a-z][\s]?[\s][0-9]{1,2}[\s][0-9:]+)(\s[0-9]{4}|)\b',
+                 'color': green_back, 'mode': 'mark_all'},
+                {'name': 'date2', 'regex': r'\b([0-9]{2}/[0-9]{2}[\s][0-9:]+)\b',
+                 'color': green_back},
+                {'name': 'IP', 'regex': r'([1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9]\.[1-2]?[0-9]?[0-9])',
+                 'color': yellow },
                 {'name': 'IP', 'regex': r'\b([a-f0-9]{4}:[a-f0-9:\.]+)\b', 'color': yellow },
-                {'name': 'env_var_windows', 'regex': r'(%[A-Z_]+%)', 'color': yellow },
-                {'name': 'env_var_linux', 'regex': r'\b($[A-Z_])\b', 'color': yellow },
+                {'name': 'env_var_windows', 'regex': r'(%[A-Z_]+%)', 'color': yellow_highlight },
+                {'name': 'env_var_linux', 'regex': r'\b($[A-Z_])\b', 'color': yellow_highlight },
+                {'name': 'var', 'regex': r'([A-Z]+_[A-Z]+)', 'color': blue },
                 {'name': 'success', 'regex': r'([Ss]ucce[a-z]+)\b', 'color': green},
                 {'name': 'failure', 'regex': r'([Ff]ail[a-z]+)\b', 'color': red},
                 {'name': 'success', 'regex': r'([Aa]ccept[a-z]+|Permit[a-z]+)\b', 'color': green},
                 {'name': 'denied', 'regex': r'([Dd]enied|[Dd]eny)\b', 'color': red},
                 {'name': 'rejected', 'regex': r'([Rr]ejected)\b', 'color': red},
+                {'name': 'warning', 'regex': r'\b([Ww]arn[a-z]*|[Aa]larm[a-z]*|[Aa]lert[a-z]*|[Aa]lert[a-z]*|'
+                                             r'[Cc]ritical[a-z]*|[Aa]ttack[a-z]*|[Ii]nject[a-z]*|[Ss]poof[a-z]*|'
+                                             r'[Dd]estruct[a-z]*|Could not [^\.]+)\b', 'color': red},
+                {'name': 'suspicious', 'regex': r'\b([Aa]ttempt[a-z]*|[Pp]ermiss[a-z]*|[Uu]nauthent[a-z]*|'
+                                                r'[Uu]nauthoriz[a-z]*|[Uu]nexpect[a-z]*|[Ss]uspiciou[a-z]*|'
+                                                r'[Ll]ockout|[Ll]ocked out|[Ee]ras[a-z]*[Ii]nfect[a-z]*|'
+                                                r'[Tt]oo [a-z]+|[Ii]nvalid[a-z]*)\b', 'color': yellow_highlight},
                 {'name': 'granted', 'regex': r'([Gg]ranted)\b', 'color': green},
                 {'name': 'blocked', 'regex': r'([Bb]locked)\b', 'color': red},
                 {'name': 'filepath_linux', 'regex': r'(/[^\s]+/[^\s]+)\b', 'color': green},
                 {'name': 'filepath_windows', 'regex': r'([C-Z]:\\[^\s]+)\b', 'color': green},
-                {'name': 'zeros', 'regex': r'\b(00|0000|\\x00)\b', 'color': grey},
+                {'name': 'zeros', 'regex': r'\b(0000|\\x00)\b', 'color': grey},
+                {'name': 'MZ_header', 'regex': r'\b(4d5a\b|4D5A\b|MZ\.\.)', 'color': blue},
+                {'name': 'PE_header', 'regex': r'(\b5045\b|\.\.PE\.\.)', 'color': blue},
+                {'name': 'bits_bytes', 'regex': r'([0-9]+ bits|[0-9]+ bytes)', 'color': green_highlight},
+                {'name': 'hashes', 'regex': r'\b([a-fA-F0-9]{32}|[a-fA-F0-9]{40}|[a-fA-F0-9]{64})\b',
+                 'color': yellow_highlight},
+                {'name': 'uid', 'regex': r'\b([a-fA-F0-9]{4,16}\-[a-fA-F0-9]{4,16}\-[a-fA-F0-9]{4,16}\-'
+                                         r'[a-fA-F0-9]{4,16}\-[a-fA-F0-9]{4,20})\b', 'color': yellow},
+                {'name': 'mac', 'regex': r'\b([a-fA-F0-9]{2}[:\-][a-fA-F0-9]{2}[:\-][a-fA-F0-9]{2}[:\-]'
+                                         r'[a-fA-F0-9]{2}[:\-][a-fA-F0-9]{2}[:\-][a-fA-F0-9]{2})\b', 'color': yellow},
+                {'name': 'base64executable',
+                 'regex': r'(TVpTAQEAAAAEAAAA//8AALgAAAA|TVoAAAAAAAAAAAAAAAAAAAAAAAA|'
+                          r'TVqAAAEAAAAEABAAAAAAAAAAAAA|TVqAAAEAAAAEABAAAAAAAAAAAAA|'
+                          r'TVpQAAIAAAAEAA8A//8AALgAAAA|TVqQAAMAAAAEAAAA//8AALgAAAA)', 'color': red},
 
                 # Security
                 {'name': 'yargen_import', 'regex': r'([A-Za-z]:\\|\.exe|\.pdb|\.scr|\.log|\.cfg|\.txt|\.dat|\.msi|\.com|\.bat|\.dll|\.pdb|\.vbs|\.tmp|\.sys)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(cmd.exe|system32|users|Documents and|SystemRoot|Grant|hello|password|process|log)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(ftp|irc|smtp|command|GET|POST|tor2web|HEAD)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(error|http[s]?|closed|version)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(Mozilla|MSIE|Windows NT|Macintosh|Gecko|Opera|User\-Agent)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(TEMP|Temporary|Appdata|Recycler)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(scan|sniff|poison|fake|spoof|sweep|dump|flood|inject|forward|scan|vulnerable|cmagentaentials|cmagentas|coded|p0c|Content)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(address[e]?[s]?|port[s]?|listen[s]?|process[e]?[s]?|service[s]?|mutex|pipe[s]?|key[s]?|lookup[s]?|connection[s]?)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(coded | c0d3d |cr3w\b)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(thawte|trustcenter|signing|class|crl|certificate|assembly)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'(cmd.exe|system32|users|Documents and|SystemRoot|Grant|hello|password|process|log|This program cannot be run in DOS mode)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(ftp|irc|smtp|command|GET|POST|tor2web|HEAD)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(error|http[s]?|closed|version)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(Mozilla|MSIE|Windows NT|Macintosh|Gecko|Opera|User\-Agent)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(TEMP|Temporary|Appdata|Recycler)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(scan|sniff|poison|fake|spoof|sweep|dump|flood|inject|forward|scan|vulnerable|cmagentaentials|cmagentas|coded|p0c|Content)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(address[e]?[s]?|port[s]?|listen[s]?|process[e]?[s]?|service[s]?|mutex|pipe[s]?|key[s]?|lookup[s]?|connection[s]?)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(coded | c0d3d |cr3w\b)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(thawte|trustcenter|signing|class|crl|certificate|assembly)', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'(yyyy|hh:mm|dd/mm|mm/dd|%s:%s:)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(cmd|pipe|tmp|temp|recycle|bin|secret|private|AppData|driver|config)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(execute|run|system|shell|root|cimv2|login|exec|stdin|read|process|netuse|script|share)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(user|pass|login|logon|token|cookie|cmagentas|hash|ticket|NTLM|LMHASH|kerberos|spnego|session|identif|account|login|privilege)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(spy|logger|dark|cryptor|RAT\b|eye|comet|evil|xtreme|poison|meterpreter|metasploit)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(cmd|pipe|tmp|temp|recycle|bin|secret|private|AppData|driver|config)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(execute|run|shell|root|cimv2|login|exec|stdin|read|process|netuse|script|share)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(user|pass|login|logon|token|cookie|cmagentas|hash|ticket|NTLM|LMHASH|kerberos|spnego|session|identif|account|login|privilege)', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'\b(spy|logger|dark|cryptor|RAT\b|eye|comet|evil|xtreme|poison|meterpreter|metasploit)', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'[\\](users|profiles|username|benutzer|Documents and Settings|Utilisateurs|Utenti|Usuários)[\\]', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'(\\\\\.\\|kernel|.dll|usage|\\DosDevices\\)', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'(loader|cmdline|ntlmhash|lmhash|drop|infect|encrypt|exec|elevat|dump|target|victim|override|traverse|mutex|pawnde|exploited|shellcode|injected|spoofed)', 'color': magenta },
-                {'name': 'yargen_import', 'regex': r'(administrator|highest|system|debug|dbg|admin|adm|root) privilege', 'color': magenta },
+                {'name': 'yargen_import', 'regex': r'(administrator|highest|SYSTEM|debug|dbg|admin|adm|root) privilege', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'(\.exe|\.dll|\.sys)$', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'(\\Release\\|\\Debug\\|\\bin|\\sbin)', 'color': magenta },
                 {'name': 'yargen_import', 'regex': r'(Management Support Team1|/c rundll32|DTOPTOOLZ Co.|net start|Exec|taskkill)', 'color': magenta },
